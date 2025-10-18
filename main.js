d3743efa431b8337885498515b21982de19960ee
Dmitriy Manjashev <dmitrymanyashev@yandex.ru>
2025-10-18 22:50:50 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let name = "Dmitrii";
+let age = 33;
+let isFalse = false;
+let car = null;
+let home = undefined;
+
+console.log(name);
+console.log(age);
+console.log(isFalse);
+console.log(car);
+console.log(home);
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isFalse));
+console.log(typeof(car));
+console.log(typeof(home));
